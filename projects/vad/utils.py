02d2be7da14b512334448f86c99f240b2dbbc5de
kallavinka8045 <h575h@naver.com>
2022-09-25 16:04:07 +0900

    디렉토리 구조변경

---
+++ projects/vad/utils.py
@@ -0,0 +1,23 @@
+import librosa
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+def load_wav(data_dir, target_sr):
+    wav, sr = librosa.load(data_dir, sr=None)  # sr=None이면 wav header에서 sr을 읽어냄
+    if sr != target_sr:
+        wav = librosa.resample(wav, orig_sr=sr, target_sr=target_sr)  # 모든 데이터의 sr을 동일하게 맞추기 위함
+    return wav
+
+def load_text(filename, split=" "):
+    with open(filename, encoding='utf-8') as f:
+        text = [line.strip().split(split) for line in f]
+    return text
+
+def plot_wav_and_label(wav, label_orig, output_filename):
+    fig = plt.figure(figsize=(16,9))
+    plt.plot(wav)
+    plt.plot(label_orig*0.3)
+    plt.savefig(os.path.splitext(output_filename)[0] + '.png')
+    plt.close()
+
